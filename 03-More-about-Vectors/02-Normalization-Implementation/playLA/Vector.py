cce87d95b6b1b7ea61140a99078b48a581f54a2f
yangqinjiang <531553280@qq.com>
2020-01-02 11:30:01 +0800

    向量的高级话题,模,标准化,

---
+++ 03-More-about-Vectors/02-Normalization-Implementation/playLA/Vector.py
@@ -0,0 +1,96 @@
+import math
+from ._globals import EPSILON
+# 定义一个py类
+class Vector:
+    def __init__(self,lst):
+        """
+        lst是一个列表
+        下划线 _ 开头的变量, 定义为私有变量
+        """
+        self._values = list(lst) #复制一份?
+
+    @classmethod
+    def zero(cls,dim):
+        """返回一个dim维的零向量"""
+        return cls([0] * dim)
+
+    def __add__(self, another):
+        """
+        向量加法,返回结果向量
+        :param another:
+        :return:
+        """
+        assert len(self) == len(another),\
+        "Error in adding. Length of vectors must be same."
+        #return Vector([a+b for a,b in zip(self._values,another._values)])
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        """向量减法,返回结果向量"""
+        assert len(self) == len(another),\
+        "Error in subtracting. Length of vectors must be same."
+
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalize(self):
+        """返回向量的单位向量"""
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError("Normalize error! norm is zero.")
+
+        return Vector(self._values) / self.norm() # 公式
+        # return 1 / self.norm() * Vector(self._values)
+        #return Vector([e /self.norm() for e in self._values])
+
+    def __mul__(self, k):
+        """返回数量乘法的结果向量: self * k"""
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        """返回数量乘法的结果向量: k * self """
+        return self * k  # 调用__mul__
+
+    def __truediv__(self, k):
+        """返回数量除尘的结果向量: self / k"""
+        return (1 / k) * self
+
+    def __pos__(self):
+        """返回向量取正的结果向量"""
+        return 1 * self # 调用__rmul__
+
+    def __neg__(self):
+        """返回向量取负的结果向量"""
+        return -1 * self # 调用__rmul__
+
+    def __iter__(self):
+        """返回向量的迭代器"""
+        return self._values.__iter__()
+
+    def __getitem__(self, index):
+        """
+        取向量的第index个元素,
+        例如: vector[1],vector[2]
+        """
+        return  self._values[index]
+
+    def __len__(self):
+        """返回向量长度(有多少个元素)"""
+        return len(self._values)
+
+    def __repr__(self):
+        """
+        在 Python 交互式命令行下直接输出对象默认使用的是__repr__
+        :return:
+        """
+        return "Vector({})" .format(self._values)
+
+    def __str__(self):
+        """
+        输出向量的字符串表达形式
+        例如: (1, 2, 3)
+        :return:
+        """
+        return "({})".format(", ".join(str(e) for e in self._values))
